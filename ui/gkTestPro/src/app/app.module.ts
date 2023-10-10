d08b92d1db156f0520934060ec9653b1d47d257a
Ketan <kda59@sfu.ca>
2023-10-10 13:40:57 -0700

    sidebar UI added to the application

--- ui/gkTestPro/src/app/app.module.ts
+++ ui/gkTestPro/src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import  {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import  {MatButtonModule} from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +13,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProjectOverviewComponent } from './project-overview/project-overview.component';
 import { ProjectTabComponent } from './project-tab/project-tab.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +21,18 @@ import { ProjectTabComponent } from './project-tab/project-tab.component';
     NavbarComponent,
     DashboardComponent,
     ProjectOverviewComponent,
-    ProjectTabComponent
+    ProjectTabComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
