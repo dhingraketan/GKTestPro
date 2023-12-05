46e2abea8198d8bd4e430fafc1d7b40979fc5e3f
Ketan <dhingraketan474@gmail.com>
2023-12-05 10:19:17 -0800

    added auth check to landing page

--- ui/gkTestPro/src/app/landing-page/landing-page.component.ts
+++ ui/gkTestPro/src/app/landing-page/landing-page.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiServiceService } from '../api-service.service';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css']
 })
-export class LandingPageComponent {
+
+export class LandingPageComponent implements OnInit{
+
+  //only show the landing page if the user is logged in
+  constructor(private apiService: ApiServiceService) { }
+
+  ngOnInit(): void {
+    this.apiService.isAutheresied().then((result: boolean) => {
+      if (result) {
+        console.log("User is Autheresied");
+      } else {
+        console.log("User is not Autheresied");
+        this.apiService.navigateToLogin();
+      }
+    });
+
+  }
+  
 
 }
