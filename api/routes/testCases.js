8f27e999ab291eaf1537f37651e85ed90903197a
Ketan <dhingraketan474@gmail.com>
2023-12-11 22:50:00 -0800

    added import tescase from xml feature

--- api/routes/testCases.js
+++ api/routes/testCases.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const uuid = require('uuid');
 const multer = require('multer');
+let fs = require('fs');
 
 const fileDir = "./uploads";
 
@@ -98,9 +99,96 @@ router.post('/import', (req, res) => {
             console.log(err);
             return res.status(501).json({ msg: "Error uploading file" });
         }
-        console.log(req.file);
+        // console.log(req.file);
         res.status(200).json({ msg: "File uploaded successfully" });
         // save the file in directory
+
+        let xmlContent = '';
+
+        // get file form directory
+
+        fs.readFile('./uploads/all_testsuites.xml', 'utf8', function (err, data) {
+            if (err) {
+                console.log(err);
+            }
+            xmlContent = data;
+
+            // parse the xml content
+            var parseString = require('xml2js').parseString;
+            parseString(xmlContent, function (err, result) {
+                //  convert result to array
+                const testSuites = result.testsuite.testsuite;
+                testCases = [];
+                testSuites.forEach(testSuite => {
+                    testcase = testSuite.testcase;
+                    testcase.forEach(test => {
+                        testCases.push(test);
+                    });
+                });
+
+                // console.log(testSuites);
+
+                // console.log(testCases);
+                testCases.forEach(async testCase => {
+                    var title = testCase.$.name;
+                    var summary = testCase.summary[0];
+                    var steps = testCase.steps[0];
+                    var expectedResult = testCase.expectedresults[0];
+                    var preCons = '';
+
+
+                    let index = summary.indexOf('Precondition');
+                    if (index != -1) {
+                        temp = summary.slice(0, index);
+                        preCons = summary.slice(index, summary.length);
+                        summary = temp;
+
+                    }
+
+                    summary = summary.replace(/<p>/g, '');
+                    summary = summary.replace(/<\/p>/g, '');
+                    // delete trailing spaces from summary
+                    summary = summary.replace(/\s+$/, '');
+                    steps = steps.replace(/<p>/g, '');
+                    steps = steps.replace(/<\/p>/g, '');
+                    expectedResult = expectedResult.replace(/<p>/g, '');
+                    expectedResult = expectedResult.replace(/<\/p>/g, '');
+                    preCons = preCons.replace(/<p>/g, '');
+                    preCons = preCons.replace(/<\/p>/g, '');
+
+                    const id = uuid.v4();
+                    const createdOn = new Date().toLocaleString();
+                    const LastModified = new Date().toLocaleString();
+                    const executionHistory = [];
+
+                    try {
+                        const newTestCase = {
+                            id,
+                            title,
+                            summary,
+                            preCons,
+                            steps,
+                            expectedResult,
+                            createdOn,
+                            LastModified,
+                            executionHistory
+                        }
+
+                        const result = await testCasdeModel.create(newTestCase);
+                        console.log(result + "Backdend")
+                        res.status(200).json({ msg: "Test case created successfully" });
+
+                    } catch (err) {
+                        res.status(500);
+                    }
+
+
+                });
+            });
+
+
+        });
+
     });
 });
 
